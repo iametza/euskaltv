d2118e30c82ed3926a0dc569f9134d4163f2bc99
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2014-04-15 16:52:24 +0200

    .gitignore eguneratu berriz

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/asier/mnt/weblanak/argia-multimedia/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/asier/mnt/weblanak/argia-multimedia/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/asier/mnt/weblanak/argia-multimedia/platforms/android/ant-build/AndroidManifest.xml : \
-/home/asier/mnt/weblanak/argia-multimedia/platforms/android/AndroidManifest.xml \
