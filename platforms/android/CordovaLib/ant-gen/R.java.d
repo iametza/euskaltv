58b88c76788a8077810b59634d55e76c20002f50
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2014-04-15 18:29:59 +0200

    .gitignorerekin arazoak berriz ere

--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /home/asier/mnt/weblanak/argia-multimedia/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
--- platforms/android/ant-gen/com/iametza/argia_multimedia/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.iametza.argia_multimedia;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
