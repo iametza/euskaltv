3a9d7108955947451dcef0ecbf6d21c2b9fe12d0
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2014-04-29 17:55:34 +0200

    Knob-aren kolorea eta tamaina aldatu

--- www/js/directives.js
+++ www/js/directives.js
@@ -6,9 +6,13 @@ angular.module('argia-multimedia-app.directives', [])
             
             // Aukera guztiak zein diren ikusteko: https://github.com/aterrien/jQuery-Knob#options
             $(element).val(scope.minLehenetsiak).knob({
-                min: 0,             // default: 0
-                max: 90,            // default: 100
-                step: 1             // default: 1
+                min: 0,                 // default: 0
+                max: 90,                // default: 100
+                step: 1,                // default: 1
+                fgColor: "#4A87EE",     // Arkuaren kolorea.
+                inputColor: "#4A87EE",  // Erdiko zenbakiaren kolorea
+                width: 300,             // Zabalera eta altuera dinamikoki ezartzea hobe litzateke ala ez? Pantaila-tamainaren arabera?
+                height: 300             
             });
         }
     };
