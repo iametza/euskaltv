833e1ca3b40aeb1225531ab567e68e7d36597903
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2014-09-05 11:42:12 +0200

    Zerbitzariaren helbidea aldatu

--- www/js/services.js
+++ www/js/services.js
@@ -6,7 +6,7 @@ angular.module('argia-multimedia-app.services', [])
 .factory('Zerbitzaria', ['$http','$q', function($http, $q) {
     var factory = {};
     
-    factory.api_url ="http://192.168.2.174/argia-multimedia-zerbitzaria/";
+    factory.api_url ="http://euskal-tv-zerbitzaria.ametza.com/";
     factory.multimedia_url = "http://argia2012.ametza.com/multimedia/";
     
     factory.alfabetikoki = [];
