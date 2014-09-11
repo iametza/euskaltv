5d3a0c0a93038c3e93c6568b06511cbab323a9f8
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2014-09-11 19:25:08 +0200

    Knob-a pantaila tamaina desberdinetara egokitzea lortu

--- www/js/directives.js
+++ www/js/directives.js
@@ -9,6 +9,22 @@ angular.module('argia-multimedia-app.directives', [])
         
         link: function(scope, element, attrs, ngModel) {
             
+            // tamaina = Altuera osoa - knob-aren div-aren marjina - 15 (Aurrera botoiaren eta beheko barraren artean tartea egon dadin)
+            // - goiko barra - beheko barra - goiko azalpena (marjina barne, horregatik true) - botoiaren div-a (marjina barne, horregatik true).
+            var tamaina = Math.round(window.screen.height - 44 - 64 - 30 - 15 - $(".goiko-azalpena").outerHeight(true) - $("#zure-erara-denbora-aurrera-botoia").outerHeight(true));
+            
+            console.log(window.screen.height);
+            console.log($(".goiko-azalpena").height());
+            console.log($("#zure-erara-denbora-aurrera-botoia").height());
+            console.log(tamaina);
+            
+            // Kalkulatutako tamaina zabaleraren %70 baino handiagoa bada hori erabiliko dugu.
+            var zabalera_max = Math.round(window.screen.width * 0.70);
+            
+            if (tamaina > zabalera_max) {
+                tamaina = zabalera_max;
+            }
+            
             // Aukera guztiak zein diren ikusteko: https://github.com/aterrien/jQuery-Knob#options
             $(element).val(scope.minutuak).knob({
                 min: 0,                 // default: 0
@@ -16,8 +32,8 @@ angular.module('argia-multimedia-app.directives', [])
                 step: 1,                // default: 1
                 fgColor: "#e6332a",     // Arkuaren kolorea.
                 inputColor: "#e6332a",  // Erdiko zenbakiaren kolorea
-                width: 200,  // Zabalera eta altuera dinamikoki ezartzea hobe litzateke ala ez? Pantaila-tamainaren arabera?
-                height: 200,
+                width: tamaina,  // Zabalera eta altuera dinamikoki ezartzea hobe litzateke ala ez? Pantaila-tamainaren arabera?
+                height: tamaina,
                 change: function(value) {
                     
                     // Hau eta beheko ngModel zergatik erabiltzen ditugun ulertzeko begiratu hau: https://groups.google.com/forum/#!msg/angular/gWqeEGK1cds/ArQAVaFmcn0J
