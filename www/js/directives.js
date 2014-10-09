325a08774498c07ed8f009e0e52cfea7199f56d9
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2014-10-09 09:53:22 +0200

    Knob-a sortzeko kodea funtzio batean sartu

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,5 +1,55 @@
 angular.module('argia-multimedia-app.directives', [])
 .directive('knob', ['ZureErara', function(ZureErara) {
+    
+    function prestatuKnob(scope, element, attrs, ngModel) {
+        
+        // tamaina = Altuera osoa - knob-aren div-aren marjina - 15 (Aurrera botoiaren eta beheko barraren artean tartea egon dadin)
+        // - goiko barra - beheko barra - goiko azalpena (marjina barne, horregatik true) - botoiaren div-a (marjina barne, horregatik true).
+        var tamaina = Math.round(window.screen.height - 44 - 64 - 30 - 15 - $(".goiko-azalpena").outerHeight(true) - $("#zure-erara-denbora-aurrera-botoia").outerHeight(true));
+        
+        console.log(window.screen.height);
+        console.log($(".goiko-azalpena").height());
+        console.log($("#zure-erara-denbora-aurrera-botoia").height());
+        console.log(tamaina);
+        
+        // Kalkulatutako tamaina zabaleraren %70 baino handiagoa bada hori erabiliko dugu.
+        var zabalera_max = Math.round(window.screen.width * 0.70);
+        
+        if (tamaina > zabalera_max) {
+            tamaina = zabalera_max;
+        }
+        
+        // Aukera guztiak zein diren ikusteko: https://github.com/aterrien/jQuery-Knob#options
+        $(element).val(scope.minutuak).knob({
+            min: 0,                 // default: 0
+            max: 90,                // default: 100
+            step: 1,                // default: 1
+            fgColor: "#e6332a",     // Arkuaren kolorea.
+            inputColor: "#e6332a",  // Erdiko zenbakiaren kolorea
+            width: tamaina,  // Zabalera eta altuera dinamikoki ezartzea hobe litzateke ala ez? Pantaila-tamainaren arabera?
+            height: tamaina,
+            change: function(value) {
+                
+                // Hau eta beheko ngModel zergatik erabiltzen ditugun ulertzeko begiratu hau: https://groups.google.com/forum/#!msg/angular/gWqeEGK1cds/ArQAVaFmcn0J
+                scope.$apply(function() {
+                    ngModel.$setViewValue(value);
+                    
+                    // Erabiltzaileak hautatutako minutuak gorde gero erabiltzeko.
+                    ZureErara.ezarriMinutuak(value);
+                    
+                    // Erabiltzaileak bideoaren gehienezko iraupena aldatu duela adierazi.
+                    ZureErara.minutuak_aldatu_dira = true;
+                });
+            }
+        });
+        
+        // Hau eta goiko scope.$apply zergatik erabiltzen ditugun ulertzeko begiratu hau: https://groups.google.com/forum/#!msg/angular/gWqeEGK1cds/ArQAVaFmcn0J
+        ngModel.$render = function(){
+           $(element).val(ngModel.$viewValue).trigger("change"); 
+        };
+        
+    }
+    
     return {
         restrict: 'A',
         
@@ -9,50 +59,7 @@ angular.module('argia-multimedia-app.directives', [])
         
         link: function(scope, element, attrs, ngModel) {
             
-            // tamaina = Altuera osoa - knob-aren div-aren marjina - 15 (Aurrera botoiaren eta beheko barraren artean tartea egon dadin)
-            // - goiko barra - beheko barra - goiko azalpena (marjina barne, horregatik true) - botoiaren div-a (marjina barne, horregatik true).
-            var tamaina = Math.round(window.screen.height - 44 - 64 - 30 - 15 - $(".goiko-azalpena").outerHeight(true) - $("#zure-erara-denbora-aurrera-botoia").outerHeight(true));
-            
-            console.log(window.screen.height);
-            console.log($(".goiko-azalpena").height());
-            console.log($("#zure-erara-denbora-aurrera-botoia").height());
-            console.log(tamaina);
-            
-            // Kalkulatutako tamaina zabaleraren %70 baino handiagoa bada hori erabiliko dugu.
-            var zabalera_max = Math.round(window.screen.width * 0.70);
-            
-            if (tamaina > zabalera_max) {
-                tamaina = zabalera_max;
-            }
-            
-            // Aukera guztiak zein diren ikusteko: https://github.com/aterrien/jQuery-Knob#options
-            $(element).val(scope.minutuak).knob({
-                min: 0,                 // default: 0
-                max: 90,                // default: 100
-                step: 1,                // default: 1
-                fgColor: "#e6332a",     // Arkuaren kolorea.
-                inputColor: "#e6332a",  // Erdiko zenbakiaren kolorea
-                width: tamaina,  // Zabalera eta altuera dinamikoki ezartzea hobe litzateke ala ez? Pantaila-tamainaren arabera?
-                height: tamaina,
-                change: function(value) {
-                    
-                    // Hau eta beheko ngModel zergatik erabiltzen ditugun ulertzeko begiratu hau: https://groups.google.com/forum/#!msg/angular/gWqeEGK1cds/ArQAVaFmcn0J
-                    scope.$apply(function() {
-                        ngModel.$setViewValue(value);
-                        
-                        // Erabiltzaileak hautatutako minutuak gorde gero erabiltzeko.
-                        ZureErara.ezarriMinutuak(value);
-                        
-                        // Erabiltzaileak bideoaren gehienezko iraupena aldatu duela adierazi.
-                        ZureErara.minutuak_aldatu_dira = true;
-                    });
-                }
-            });
-            
-            // Hau eta goiko scope.$apply zergatik erabiltzen ditugun ulertzeko begiratu hau: https://groups.google.com/forum/#!msg/angular/gWqeEGK1cds/ArQAVaFmcn0J
-            ngModel.$render = function(){
-               $(element).val(ngModel.$viewValue).trigger("change"); 
-            };
+            prestatuKnob(scope, element, attrs, ngModel);
         }
     };
 }])
